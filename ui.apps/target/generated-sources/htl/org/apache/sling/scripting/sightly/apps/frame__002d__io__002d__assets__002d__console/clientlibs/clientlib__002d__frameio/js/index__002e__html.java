d3c4018592dbf913dc340cd8dbc25a994549d88d
arjena <arjena@adobe.com>
2024-11-19 16:11:03 -0700

    comment

---
+++ ui.apps/target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/frame__002d__io__002d__assets__002d__console/clientlibs/clientlib__002d__frameio/js/index__002e__html.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ ******************************************************************************/
+package org.apache.sling.scripting.sightly.apps.frame__002d__io__002d__assets__002d__console.clientlibs.clientlib__002d__frameio.js;
+
+import java.io.PrintWriter;
+import java.util.Collection;
+import javax.script.Bindings;
+
+import org.apache.sling.scripting.sightly.render.RenderUnit;
+import org.apache.sling.scripting.sightly.render.RenderContext;
+
+public final class index__002e__html extends RenderUnit {
+
+    @Override
+    protected final void render(PrintWriter out,
+                                Bindings bindings,
+                                Bindings arguments,
+                                RenderContext renderContext) {
+// Main Template Body -----------------------------------------------------------------------------
+
+out.write("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"/><title>Static Frontend file</title><meta name=\"template\" content=\"page-content\"/><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"/><script defer=\"defer\" src=\"/main.js\"></script></head><body class=\"page\"><div id=\"frame-io-quickpublish-dialog\" style=\"width:100%;\"></div></body></html>");
+
+
+// End Of Main Template Body ----------------------------------------------------------------------
+    }
+
+
+
+    {
+//Sub-Templates Initialization --------------------------------------------------------------------
+
+
+
+//End of Sub-Templates Initialization -------------------------------------------------------------
+    }
+
+}
+
